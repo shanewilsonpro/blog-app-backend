adbf2b34353a3e64aa93fb9344f150200eadafaa
Shane Wilson <shanewilsonpro@gmail.com>
2022-09-20 12:37:05 -0700

    created user model and schema

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const dotenv = require('dotenv');
+
+dotenv.config();
+const dbConnect = require('./config/database/dbConnect');
 
 const app = express();
 
+// database connect
+dbConnect();
+
+// server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running... ${PORT}`));
